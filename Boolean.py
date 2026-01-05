7962611dc88659e8fbf5235651741b21b08c6777
Chairil <dirgaakbar170805@gmail.com>
2026-01-05 23:32:16 +0900

    True dan False

---
+++ Boolean.py
@@ -0,0 +1,12 @@
+# BOOLEAN ADALAH TRUE DAN FALSE
+sekarang_hujan = True
+if sekarang_hujan:
+      print('Bawa payung')
+else:
+      print('Tidak perlu bawa payung')
+
+sekarang_malam = False
+if sekarang_malam:
+      print('Waktunya tidur')
+else:
+      print('Belum saatnya tidur')
