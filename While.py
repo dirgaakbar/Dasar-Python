951ce0ee516c3e27a8228d7cf7944df150de316c
Akbar <dirgaakbar170805@gmail.com>
2026-01-07 00:25:33 +0900

    For-While.py

--- While.py
+++ While.py
@@ -1,5 +1,9 @@
+# FOR ADALAH LOOP YANG SUDAH KITA KETAHUI HASILNYA
+for i in range(5):	# Angkanya akan mulai dari o hingga 4
+	print(i)
+
 # WHILE ADALAH LOOP DENGAN SYARAT
 angka = 1		# Angkanya mulai dari 1
 while angka <= 5:	# Jika angkanya sama atau kurang dari 5 artinya True
-	print(angka)	
+	print(angka)
 	angka += 1	# Programnya tambah 1 hingga mencapai 5
