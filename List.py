6f5af84385e1acf56b89f538f248bce90dc9ee54
Chairil <dirgaakbar170805@gmail.com>
2026-01-19 21:10:51 +0900

    up

--- List.py
+++ List.py
@@ -1,3 +1,9 @@
 # List bisa berisi string,angka,dan campur
-buah =['Apel', 'Mangga', 'Pisang'] # nilainy dimualai dari 0
+buah =['Apel', 'Mangga', 'Pisang'] # index dimulai dari  0
+buah[1] = 'Jeruk' # Mengubah isi list
 
+print(buah[0]) # Menampilkan Apel
+print(buah[1])
+
+buah.append('anggur') # Menambahkan data di list terakhir
+buah.insert(1, 'nanas') # Menambahkan data di posisi tertentu 
