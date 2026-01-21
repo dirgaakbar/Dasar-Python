3dc7f5389651f4df7b3e95c6ac85cd904e88a802
Chairil <dirgaakbar170805@gmail.com>
2026-01-21 21:55:47 +0900

    Tebak angka

--- Tugas-3.py
+++ Tugas-3.py
@@ -2,16 +2,13 @@
 import random
 
 angka_rahasia = random.randint(1, 10)
-tebakan = 1
+tebakan = int(input('Masukkan angka tebakan (1-10):  '))
 
-print('Saya memikirakan angka antara 1 sampai 10. bisa tebak?')
-
-while tebakan != angka_rahasia    # Akan terus mengulang hingga jawabannya == angka_rahasia
-    tebakan = int(input('Masukkan tebakanmu:    '))
-
-if tebakan < angka_rahasia:
-    print('Terlalu rendah! coba lagi')
-elif tebakan > angka_rahasia:
-    print('Terlalu tinggi! coba lagi')
-else:
-    print('Selamat! kamu berhasil menebaknya')
+while tebakan != angka_rahasia:
+    tebakan = int(input('Coba lagi:    '))
+    if tebakan < angka_rahasia:
+        print('Tebakan terlalu kecil')
+    elif tebakan > angka_rahasia:
+        print('Tebakan terlalu besar')
+    else:
+        print('Selamat! tebakan kamu benar')
