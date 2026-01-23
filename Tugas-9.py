6698da3afa399df869794c6a116403c97bb4305f
Chairil <dirgaakbar170805@gmail.com>
2026-01-23 21:35:10 +0900

    Lagu anak ayam

---
+++ Tugas-9.py
@@ -0,0 +1,8 @@
+# Perulangan lagu anak ayam
+for i in range(99, 0, -1):
+    print('Tek kotek, kotek, kotek')
+    print('Anak ayam turun berkotek')
+    print('')
+    print(f'Anak ayam turunlah {i}')
+    print(f'Mati satu tinggallah {i-1}')
+    print('')
