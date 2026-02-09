6e517109157f2272c874153e90226110d28e0cf3
Chairil <dirgaakbar170805@gmail.com>
2026-02-09 15:29:26 +0900

    Menyimpan data dalam bentuk Key : value

--- Dictionary.py
+++ Dictionary.py
@@ -4,3 +4,11 @@ pelajar = {
 	'umur': 18,
 	'jurusan': 'Kelistrikan'
 }
+print(pelajar['nama']) # Cara print Dictionary
+
+pelajar['alamat'] = 'Sentani' # Menambah data alamat
+
+pelajar['umur'] = 20 # Mengubah data umur
+
+#del pelajar['alamat'] # Menghapus data alamat
+#pelajar.clear() # Menghapus semua data
